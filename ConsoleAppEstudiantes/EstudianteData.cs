94b9c47a64fa4a8a7bb22a81754031141269be44
Armax <armanmv518@gmail.com>
2024-10-08 03:25:22 -0600

    Initial commit

---
+++ ConsoleAppEstudiantes/EstudianteData.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using System.Data.SqlClient;
+
+public static class EstudianteData
+{
+    /// <summary>
+    /// Conexión a la BD
+    /// </summary>
+    private static string connectionString = "Server=.\\SQLExpress;Database=Evaluacion;Trusted_Connection=true;TrustServerCertificate=true";
+
+    /// <summary>
+    /// Método para obtener todos los estudiantes 
+    /// </summary>
+    /// <returns>Devuelve estudiantes</returns>
+    public static IEnumerable<Estudiante> ObtenerEstudiantes()
+    {
+        try
+        {
+            using (var conexion = new SqlConnection(connectionString))
+            {
+                conexion.Open();
+                string stored = "spObtenerEstudiantes";
+                return conexion.Query<Estudiante>(stored).ToList();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+            return Enumerable.Empty<Estudiante>();
+        }
+    }
+}
---
+++ ConsoleAppEstudiantes/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        var estudiantes = EstudianteData.ObtenerEstudiantes();
+
+        foreach (var estudiante in estudiantes)
+        {
+            Console.WriteLine($"Nombre: {estudiante.Nombre} - Edad: {estudiante.Edad}");
+        }
+    }
+}
